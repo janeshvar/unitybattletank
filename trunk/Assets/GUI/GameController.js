c55f1cfeb6cae83dc03a88cf6633bc15a7d6188e
cmorwin <cmorwin@ff5e9423-e3ca-ca59-4b8f-16e7f816d8d8>
2010-12-04 15:31:58 +0000

    Added GUI stuff to track the tank health and scores.  Having problems with the script and getting it to look up the tanks health.

--- trunk/Assets/GUI/GameController.js
+++ trunk/Assets/GUI/GameController.js
@@ -1,8 +1,16 @@
+@script ExecuteInEditMode()
+
 var gameTimer : float = 120.0;
 // Game timer in seconds
 
 
 var gameTimerTextScale : float = 1.5;
+var playerNameTextScale : float = 1.0;
+var playerScoreTextScale : float = 1.0;
+var tank1 : GameObject;
+var tank2 : GameObject;
+private var tankDamage1 : TankDamage = tank1.GetComponent("TankDamage");
+private var tankDamage2 : TankDamage;
 var GUICustomSkin : GUISkin;
 
 
@@ -16,6 +24,11 @@ function Update () {
 		Application.LoadLevel("GameOver");
 }
 
+function OnLoad() {
+	//tankDamage1 = tank1.GetComponent("TankDamage");
+	tankDamage2 = tank2.GetComponent("TankDamage");
+}
+
 
 function OnGUI() {
 	if (GUICustomSkin)
@@ -26,7 +39,26 @@ function OnGUI() {
 	
 	var minutes : int = gameTimer / 60;
 	var seconds : int = gameTimer - (60 * minutes);
-	var timerGUI : String = minutes.ToString() + ":" + seconds.ToString();
-	GUI.Label(Rect(10, 10, 50, 50), timerGUI, "MenuText");
+	var timerGUI : String = minutes.ToString() + ":";
+	if(seconds < 10) {
+		timerGUI = timerGUI + "0" + seconds.ToString();
+	} else {
+		timerGUI = timerGUI + seconds.ToString();
+	}
+	GUI.Label(Rect(5, 10, 50, 50), timerGUI, "MenuText");
+	
+	GUI.matrix = Matrix4x4.TRS(Vector3(0, 0, 0), Quaternion.identity, Vector3.one * playerNameTextScale);
+	var tankDamage11 : TankDamage;
+	tankDamage11 = tank1.GetComponent("TankDamage");
+	var p1Health : String = "Health: " + tankDamage11.health;
+	var p1Score : String = "Score: ";
+	//var p2Health : String = "Health: " + tankDamage2.health;
+	var p2Score : String = "Score: ";
+	GUI.Label(Rect(5, 100, 50, 50), "Player 1", "MenuText");
+	GUI.Label(Rect(5, 120, 50, 50), p1Health, "MenuText");
+	GUI.Label(Rect(5, 140, 50, 50), p1Score, "MenuText");
+	GUI.Label(Rect(5, 250, 50, 50), "Player 2", "MenuText");
+	//GUI.Label(Rect(5, 270, 50, 50), p2Health, "MenuText");
+	GUI.Label(Rect(5, 290, 50, 50), p2Score, "MenuText");
 	
 }
