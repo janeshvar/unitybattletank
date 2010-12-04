12feea19ed74091433e6a6795de7fb82153326cf
cmorwin <cmorwin@ff5e9423-e3ca-ca59-4b8f-16e7f816d8d8>
2010-12-04 15:44:04 +0000

    Fixed the GameController script to properly display the GUI for player healths.

--- trunk/Assets/GUI/GameController.js
+++ trunk/Assets/GUI/GameController.js
@@ -9,7 +9,7 @@ var playerNameTextScale : float = 1.0;
 var playerScoreTextScale : float = 1.0;
 var tank1 : GameObject;
 var tank2 : GameObject;
-private var tankDamage1 : TankDamage = tank1.GetComponent("TankDamage");
+private var tankDamage1 : TankDamage;
 private var tankDamage2 : TankDamage;
 var GUICustomSkin : GUISkin;
 
@@ -22,11 +22,6 @@ function Update () {
 	
 	if(gameTimer <= 0)
 		Application.LoadLevel("GameOver");
-}
-
-function OnLoad() {
-	//tankDamage1 = tank1.GetComponent("TankDamage");
-	tankDamage2 = tank2.GetComponent("TankDamage");
 }
 
 
@@ -48,17 +43,21 @@ function OnGUI() {
 	GUI.Label(Rect(5, 10, 50, 50), timerGUI, "MenuText");
 	
 	GUI.matrix = Matrix4x4.TRS(Vector3(0, 0, 0), Quaternion.identity, Vector3.one * playerNameTextScale);
-	var tankDamage11 : TankDamage;
-	tankDamage11 = tank1.GetComponent("TankDamage");
-	var p1Health : String = "Health: " + tankDamage11.health;
+	if(!tankDamage1) {
+		tankDamage1 = tank1.GetComponent("TankDamage");
+	}
+	if(!tankDamage2) {
+		tankDamage2 = tank2.GetComponent("TankDamage");
+	}
+	var p1Health : String = "Health: " + tankDamage1.health;
 	var p1Score : String = "Score: ";
-	//var p2Health : String = "Health: " + tankDamage2.health;
+	var p2Health : String = "Health: " + tankDamage2.health;
 	var p2Score : String = "Score: ";
 	GUI.Label(Rect(5, 100, 50, 50), "Player 1", "MenuText");
 	GUI.Label(Rect(5, 120, 50, 50), p1Health, "MenuText");
 	GUI.Label(Rect(5, 140, 50, 50), p1Score, "MenuText");
 	GUI.Label(Rect(5, 250, 50, 50), "Player 2", "MenuText");
-	//GUI.Label(Rect(5, 270, 50, 50), p2Health, "MenuText");
+	GUI.Label(Rect(5, 270, 50, 50), p2Health, "MenuText");
 	GUI.Label(Rect(5, 290, 50, 50), p2Score, "MenuText");
 	
 }
